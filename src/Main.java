579b1b769dd72bae377e3324bdc025129cce2418
Anant2609 <ananttamrakar14@gmail>
2024-04-15 14:20:17 +0530

    world

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 10;
+        int y = 11;
+        System.out.println(x+y);
+        System.out.println("Hello world!");
+    }
+}
